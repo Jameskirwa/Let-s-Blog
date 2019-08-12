1218a7011d19814600141c6a22b43a9dbe2fe65c
James Kirwa <example@email.com>
2019-08-12 09:50:13 +0300

    create a virtual env

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/denis1816/Desktop/Pizza/virtual"
+set -gx VIRTUAL_ENV "/home/james/Documents/moringa-school-projects/Let's blog/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
